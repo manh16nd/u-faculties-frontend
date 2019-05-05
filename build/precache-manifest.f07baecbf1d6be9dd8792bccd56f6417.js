a9390522a4eddbd45556190947baaa1f9161c880
Nguyễn Đức Mạnh <manh16nd@gmail.com>
2019-05-06 05:55:54 +0700

    Build

---
+++ build/precache-manifest.f07baecbf1d6be9dd8792bccd56f6417.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "1309acbb842e32e2e9a6",
+    "url": "/static/css/main.f7ef001d.chunk.css"
+  },
+  {
+    "revision": "1309acbb842e32e2e9a6",
+    "url": "/static/js/main.3c2b2a79.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "3acfc93b4ffde23782b4",
+    "url": "/static/css/2.4ced5be4.chunk.css"
+  },
+  {
+    "revision": "3acfc93b4ffde23782b4",
+    "url": "/static/js/2.6f820730.chunk.js"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/static/media/themify.e23a7dca.ttf"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/static/media/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "f6cc0ab9c02b74d31130de1710204be9",
+    "url": "/index.html"
+  }
+];
--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = process.env.REACT_APP_BACKEND
+const baseUrl = 'https://u-faculties-backend.herokuapp.com'
 
 export const createApiService = async ({url, method, data, params}) => {
     try {
