547a7ead751938d5b3446b2e319767916418a978
Nguyễn Đức Mạnh <manh16nd@gmail.com>
2019-05-21 00:44:31 +0700

    update

--- src/services/cookies/index.js
+++ src/services/cookies/index.js
@@ -5,3 +5,7 @@ const cookies = new Cookies()
 export const setCookie = (name, value, options) => {
     cookies.set(name, value, options)
 }
+
+export const getCookie = (name) => {
+    return cookies.get(name) || ''
+}
