0c455071e1a5f4df63c8392c754ecf06de129385
Nguyễn Đức Mạnh <manh16nd@gmail.com>
2019-05-24 21:46:10 +0700

    sticky table

--- src/containers/teachers/components/Teachers.js
+++ src/containers/teachers/components/Teachers.js
@@ -1,19 +1,62 @@
 import React, {Component} from 'react'
+import {getTeachers} from '../../../services/api/TeachersServices'
 
 class Teachers extends Component {
+    state = {
+        teachers: {
+            teachers: [],
+            total: 0,
+            limit: 0,
+            page: 1,
+            loading: false,
+        },
+        current: {
+            open: false,
+
+        }
+    }
+
+    componentDidMount() {
+        this._fetchTeachers()
+    }
+
+    _fetchTeachers = async () => {
+        const {teachers} = this.state
+        const {limit, page} = teachers
+        const {data, message} = await getTeachers({limit, page})
+        if (message) return alert(message)
+        this.setState({
+            teachers: {
+                ...teachers,
+                total: data.total,
+                teachers: data.teachers,
+            }
+        })
+    }
+
+    _onClickNewTeacher = () => {
+        this.setState({
+            current: {
+                open: false,
+                teacher: {},
+            }
+        })
+    }
+
     render() {
+        const {teachers} = this.state
 
         return (
             <div className="Teachers">
                 <div className="TopButtons">
-                    <button className="UserButton">Thêm giảng viên</button>
+                    <button className="UserButton" onClick={this._onClickNewTeacher}>Thêm giảng viên</button>
                 </div>
 
                 <div className="TableWrapper">
                     <table className="Table">
                         <thead>
                         <tr>
-                            <th>Tên giảng viên</th>
+                            <th className="FixedColumn">Tên giảng viên</th>
                             <th>Email</th>
                             <th>VNU Email</th>
                             <th>Điện thoại</th>
@@ -21,8 +64,24 @@ class Teachers extends Component {
                             <th>Website</th>
                             <th>Bằng cấp</th>
                             <th>Vị trí</th>
+                            <th>Hành động</th>
                         </tr>
                         </thead>
+                        <tbody>
+                        {teachers.teachers.map((teacher) => <tr key={teacher._id}>
+                            <td className="FixedColumn">{teacher.name}</td>
+                            <td>{teacher.email}</td>
+                            <td>{teacher.vnuEmail}</td>
+                            <td>{teacher.phone}</td>
+                            <td>{teacher.address}</td>
+                            <td><a href={teacher.website} rel="noopener noreferrer"
+                                   target="_blank">{teacher.website}</a>
+                            </td>
+                            <td>{teacher.degree}</td>
+                            <td>{teacher.position}</td>
+                            <td></td>
+                        </tr>)}
+                        </tbody>
                     </table>
                 </div>
             </div>
