1e88ea633576b3a33062163c55e5a6160099e1dd
Nguyễn Đức Mạnh <manh16nd@gmail.com>
2019-05-26 20:52:18 +0700

    init layout

--- src/containers/teacher-topics/components/TeacherTopics.js
+++ src/containers/teacher-topics/components/TeacherTopics.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import classnames from 'classnames'
 
 const TeacherTopics = function (props) {
+    const [tab, changeTab] = useState(0)
+
+    const _changeTab = (value) => () => {
+        changeTab(value)
+    }
 
     return (
         <div className="TeacherTopics container">
@@ -8,8 +14,16 @@ const TeacherTopics = function (props) {
                 <div className="CardHeader">
                     Quản lý chủ đề nghiên cứu
                 </div>
-                <div className="CardBody">
+            </div>
 
+            <div className="FieldsWrapper">
+                <div className="Tabs">
+                    <div className={classnames("Tab", { 'Focus': !tab })} onClick={_changeTab(0)}>
+                        Các chủ đề đang tham gia
+                    </div>
+                    <div className={classnames("Tab", { 'Focus': !!tab })} onClick={_changeTab(1)}>
+                        Tất cả chủ đề
+                    </div>
                 </div>
             </div>
         </div>
